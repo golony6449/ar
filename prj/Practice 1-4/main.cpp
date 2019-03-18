0955f1dab9282161f03192bb43e1f9c110ce3684
박성흠 <ab6449@gmail.com>
2019-03-18 11:52:07 +0900

    카메라롤 QR코드 인식

--- prj/Practice 1-4/main.cpp
+++ prj/Practice 1-4/main.cpp
@@ -9,16 +9,26 @@ const string dataPath = "../data/out/";
 void main()
 {
 	VideoCapture capture(1);
-	Mat frame;
+	QRCodeDetector detector;
+
+	Mat frame, points, qrcode;
+	string data;
 	int num = 1;
 
 	while (true) {
+		// 화면 출력
 		//capture.read(frame);
 		capture >> frame;
-
 		imshow("cam", frame);
-		imwrite(dataPath + to_string(num) + ".jpg", frame);
+
+		data = detector.detectAndDecode(frame, points, qrcode);
+		if (data.length() > 0) {
+			imshow("qrcode", qrcode);
+			cout << data << endl;
+		}
+
+		//imwrite(dataPath + to_string(num) + ".jpg", frame);
+		//num++;
 		waitKey(1);
-		num++;
 	}
 }
