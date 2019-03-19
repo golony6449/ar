13d5b3999b665ad0b4f804fa4e98467996f19076
박성흠 <ab6449@gmail.com>
2019-03-20 00:44:35 +0900

    QR 코드 인식 후 마킹

--- prj/Practice 1-4/main.cpp
+++ prj/Practice 1-4/main.cpp
@@ -22,13 +22,39 @@ void main()
 		imshow("cam", frame);
 
 		data = detector.detectAndDecode(frame, points, qrcode);
+
 		if (data.length() > 0) {
-			imshow("qrcode", qrcode);
 			cout << data << endl;
+
+			putText(frame, data, Point((int)points.at<float>(0, 0), (int)points.at<float>(0, 1)), 
+				FONT_HERSHEY_PLAIN, 20, Scalar(255, 0, 0));
+
+			line(frame, Point((int)points.at<float>(1, 0), (int)points.at<float>(1, 1)), 
+				Point((int)points.at<float>(2, 0), (int)points.at<float>(2, 1)),
+				Scalar(0, 255, 255), 3);
+
+			line(frame, Point((int)points.at<float>(2, 0), (int)points.at<float>(2, 1)),
+				Point((int)points.at<float>(3, 0), (int)points.at<float>(3, 1)),
+				Scalar(255, 0, 255), 3);
+
+			line(frame, Point((int)points.at<float>(0, 0), (int)points.at<float>(0, 1)),
+				Point((int)points.at<float>(1, 0), (int)points.at<float>(1, 1)),
+				Scalar(255, 255, 0), 3);
+
+			line(frame, Point((int)points.at<float>(3, 0), (int)points.at<float>(3, 1)),
+				Point((int)points.at<float>(1, 0), (int)points.at<float>(1, 1)),
+				Scalar(0, 0, 255), 3);
+			
+		//	for (int i = 0; i < 3; i++)
+		//		line(frame, Point((int)points.at<float>(i, 0), (int)points.at<float>(i, 1)), 
+		//			Point((int)points.at<float>(i + 1, 0), (int)points.at<float>(i + 1, 1)),
+		//			Scalar(255, 255, 255));
 		}
 
 		//imwrite(dataPath + to_string(num) + ".jpg", frame);
 		//num++;
+
+		imshow("qrcode", qrcode);
 		waitKey(1);
 	}
 }
